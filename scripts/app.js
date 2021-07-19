1b11f8470ac402d0e4f69a59d4b9395102c81091
Victor <vjmreysantos@gmail.com>
2021-07-19 10:32:11 +0100

    game project

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
-// * ELEMENTS
 const grid = document.querySelector('.grid')
 const tiles = []
+const newGame = document.querySelector('.new-game')
+const scoreDisplay = document.querySelector('#score-value')
 
 
 
@@ -12,12 +13,13 @@ const tileCount = width * width
 
 // * GAME VARIABLES
 const materClass = 'mater'
+const mcqueenClass = 'mcqueen'
 let materPosition = 51
-
+let mcqueenPosition = 57
+let score = 0
 
 
 // * FUNCTIONS
-
 
 function addMater() {
   tiles[materPosition].classList.add(materClass)
@@ -25,6 +27,14 @@ function addMater() {
 function removeMater() {
   tiles[materPosition].classList.remove(materClass)
 }
+
+function addMcqueen() {
+  tiles[mcqueenPosition].classList.add(mcqueenClass)
+}
+function removeMcqueen() {
+  tiles[mcqueenPosition].classList.remove(mcqueenClass)
+}
+
 function createGrid() {
   for (let i = 0; i < tileCount; i++) {
     const tile = document.createElement('div')
@@ -33,10 +43,28 @@ function createGrid() {
     tiles.push(tile)
   }
 }
+
+function handleNewGame() {
+  setInterval(() => {
+    tiles[materPosition].classList.add('mater')
+    materPosition++
+  }, 500)
+}
+
+function handleMcqueen() {
+  setInterval(() => {
+    tiles[mcqueenPosition].classList.remove('mcqueen')
+    mcqueenPosition = Math.floor(Math.random() * tileCount)
+    tiles[mcqueenPosition].classList.add('mcqueen')
+  }, 2000)
+}
+
+
 function handleKeyUp(event) {
   const x = materPosition % width
   const y = Math.floor(materPosition / width)
   removeMater()
+
   switch (event.keyCode) {
     case 39:
       if (x < width - 1) {
@@ -60,14 +88,15 @@ function handleKeyUp(event) {
       break
   }
   addMater()
+  addMcqueen()
 }
 createGrid()
 addMater()
-
-
+addMcqueen()
 
 
 
 
 // * EVENTS
 window.addEventListener('keyup', handleKeyUp)
+newGame.addEventListener('click', handleNewGame, handleMcqueen)
